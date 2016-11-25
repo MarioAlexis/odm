7591e88249811c663ec6be14c2c672022b61ec7c
root <root@mario-linux>
2016-11-25 11:35:49 -0500

    add script for stats

--- scripts/call_odm.sh
+++ scripts/call_odm.sh
@@ -1,3 +1,23 @@
 #/bin/bash
 
-python run.py --project-path $1
+file=ODM_stats_default.txt
+bench=benchmark.txt
+path=$1
+echo -e "/****************************" > $file
+echo -e "/*" >> $file
+echo -e "/*       Running ODM with default setting" >> $file
+echo -e "/*" >> $file
+echo -e "/****************************" >> $file
+echo -e " " >> $file
+
+for i in {0..1}
+do
+	sleep 1
+	python run.py --time --project-path $path
+	sleep 5
+	echo -e "*-*-*-$i" >> $file
+	cat $path$bench >> $file
+	echo -e " " >> $file
+	sleep 3
+	bash clean_test.sh
+done
