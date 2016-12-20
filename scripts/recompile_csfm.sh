ed0aee8fd1deca708fb3bbb6cd0c41acffad1304
David St-Onge <david.st-onge@polymtl.ca>
2016-12-20 16:44:49 -0500

    change recompile script

--- scripts/recompile_csfm.sh
+++ scripts/recompile_csfm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf */
-find . ! -name 'recompile.sh' -type f -exec rm -f {} +
+find . ! -name 'recompile_csfm.sh' -type f -exec rm -f {} +
 cmake -D CERES_ROOT_DIR=../../../../install/ ..
 make
