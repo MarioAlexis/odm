edc656b939b372974645afc6cd357f91eb71b7a9
Mario Alexis Labrana <mario-alexis.labrana-ruiz@polymtl.ca>
2016-12-21 09:24:57 -0500

    better scripts

--- scripts/reduceNumPic.sh
+++ scripts/reduceNumPic.sh
@@ -8,7 +8,7 @@ counter=0
 if [ $srcpath == "--help" ]
 then
 	echo
-	echo -e "1.- Source Path"
+	echo -e "1.- Source Path - where pictures are storage"
 	echo -e "2.- Destination Path"
 	echo -e "3.- Dividor Factor"
 	echo
@@ -22,13 +22,18 @@ then
 	mkdir -p $dstpath/images
 fi
 
+echo -e " $(ls -l $srcpath | wc -l) images in SRC folder"
+echo -e " Dividor factor : $factor"
+
 for i in $srcpath/*
 do
 	filename=$(basename "$i")
 	if [ $counter == $(($factor)) ]
 	then
-		cp $srcpath/$filename $dstpath/
+		cp $srcpath/$filename $dstpath/images
 		counter=0
 	fi
 	counter=$(($counter+1))
 done
+
+echo -e " $(ls -l $dstpath/images | wc -l) images in DST folder"
--- scripts/stats_odm.sh
+++ scripts/stats_odm.sh
@@ -1,23 +1,37 @@
 #/bin/bash
 
-file=ODM_stats_default.txt
-bench=stats_benchmark.txt
+file=config08.txt
+zipfile=config_08.zip
+bench=benchmark.txt
 path=$1
 echo -e "/****************************" > $file
 echo -e "/*" >> $file
-echo -e "/*       Running ODM with default setting" >> $file
+echo -e "/*       Running Config -- 08 -- " >> $file
 echo -e "/*" >> $file
 echo -e "/****************************" >> $file
 echo -e " " >> $file
 
 for i in {0..29}
 do
-	sleep 1
-	python run.py --time --resize-to 1280 --min-num-features 2000 --project-path $path
-	sleep 5
-	echo -e "*-*-*-$i" >> $file
-	cat $path$bench >> $file
-	echo -e " " >> $file
-	sleep 3
-	bash clean_test.sh
+	if [ $i == 29 ]
+	then
+		python run.py --time --resize-to 1280 --min-num-features 2000 --odm_meshing-maxVertexCount 100000 --project-path $path
+		sleep 5
+		echo -e "*-*-*-$i" >> $file
+		cat $path$bench >> $file
+		echo -e " " >> $file
+		sleep 3
+		zip $zipfile $file $pathodm_texturing/*.*
+		sleep 1
+		mv $zipfile /home/ubuntu/
+	else
+		sleep 1
+		python run.py --time --resize-to 1280 --min-num-features 2000 --odm_meshing-maxVertexCount 100000 -s resize -e odm_meshing --project-path $path
+		sleep 5
+		echo -e "*-*-*-$i" >> $file
+		cat $path$bench >> $file
+		echo -e " " >> $file
+		sleep 3
+		bash clean_test.sh $path
+	fi
 done
